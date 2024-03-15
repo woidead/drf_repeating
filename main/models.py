240c437db107ebf46e8d81085c28f2b1d03dded3
woidead <abikbest05@gmail.com>
2024-03-15 16:31:23 +0600

    created models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Celebrity(models.Model):
+    title = models.CharField("Имя Фамилия", max_length=255)
+    content = models.TextField("Информация", blank=True)
+    time_create = models.DateTimeField("Время создания", auto_now_add=True)
+    time_update = models.DateTimeField("Время изменения", auto_now_add=True)
+    is_published = models.BooleanField("Опубликована", default=True)
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, null=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+
+class Category(models.Model):
+    name = models.CharField("Название категории", max_length=100, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
