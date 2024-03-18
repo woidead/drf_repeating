cd399b379b35fc07664e1606b08b42b3b081b5ec
woidead <abikbest05@gmail.com>
2024-03-18 15:35:58 +0600

    made post request

--- main/views.py
+++ main/views.py
@@ -1,9 +1,23 @@
-from rest_framework import generics
 from .models import Celebrity, Category
 from .serializers import CelebritySerializer
+from django.forms import model_to_dict
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
+# class CelebrityAPIView(generics.ListAPIView):
+#     queryset = Celebrity.objects.all()
+#     serializer_class = CelebritySerializer
 
-class CelebrityAPIView(generics.ListAPIView):
-    queryset = Celebrity.objects.all()
-    serializer_class = CelebritySerializer
+class CelebrityAPIView(APIView):
+    def get(self, request):
+        data = Celebrity.objects.all().values()
+        return Response({'data': list(data)})
     
+    def post(self, request):
+        celeba = Celebrity.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            category_id=request.data['category_id']
+        )
+        return Response({"data":model_to_dict(celeba)})
